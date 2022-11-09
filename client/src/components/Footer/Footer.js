f3483c559205d34be9cb52d879e85e0ca2526350
BryceOph <111711735+BryceOph@users.noreply.github.com>
2022-11-09 14:26:26 -0600

    worked on footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto text-dark p-4">
+      <div className="container text-center mb-5">
+        <h4>&copy; {new Date().getFullYear()} - Haunted House</h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
