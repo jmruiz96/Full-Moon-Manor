1d61e5758c200a6f489484f707ef1cbc5aadc494
Matthew Bronstein <mbronstein1@gmail.com>
2022-11-09 14:18:26 -0600

    fixed user seed structure

--- server/seeders/userSeed.js
+++ server/seeders/userSeed.js
@@ -3,8 +3,12 @@ module.exports = [
         name: 'Matt Bronstein',
         email: "test@test.com",
         password: '12345678',
-        adventures: [["Torture Room", "Furnace", "Crypt", "Elevator", "First Floor Landing", "Study"],
-        ["Torture Room", "Wine Cellar", "Store Room"]]
+        adventures: [{
+            roomNames: ["Torture Room", "Furnace", "Crypt", "Elevator", "First Floor Landing"]
+        },
+        {
+            roomNames: ["Torture Room", "Wine Cellar", "Store Room"]
+        }]
     },
     {
         name: 'test1',
