6bde706624c04645034ad01fa4143fc0cff51891
Matthew Bronstein <mbronstein1@gmail.com>
2022-11-10 11:19:31 -0600

    figured out logic for rendering page based on event or not

--- client/src/pages/Room/Room.js
+++ client/src/pages/Room/Room.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_ROOM, QUERY_ME } from '../../utils/queries';
 import Auth from '../../utils/auth'
 
 export const Room = () => {
+    // const [roomState, setRoomState] = useState('Torture Room')
+    // const [ lifeCount, setLifeCount ] = 10;
+    // const [ eventResolution, setEventResolution ] = "";
+
     const { roomName } = useParams();
 
     const { loading, data } = useQuery(QUERY_ROOM, {
         variables: { roomName }
     });
 
-    // console.log(data)
+    console.log(data)
     const room = data?.room || {};
 
     if (loading) {
@@ -20,11 +24,21 @@ export const Room = () => {
     return (
         <>
             {
-                Auth.loggedIn() ?
-                    <div>
-                        <h1>{room.roomName}</h1>
-                    </div> :
-                    <p>"Must be logged in"</p>
+                !Auth.loggedIn() ?
+                    (<p>'You must be logged in!</p>
+                    ) : (
+                        // Checking if room has no event or event, render different XML
+                        Auth.loggedIn() && room.event.length === 0 ?
+                            // If room has no event, render this 
+                            <div>
+                                <h1>{`${room.roomName} has no event`}</h1>
+                            </div> :
+                            // If room has event, render this
+                            <div>
+                                <h1>{`${room.roomName} has an event`}</h1>
+                            </div>
+                    )
+
             }
         </>
     )
