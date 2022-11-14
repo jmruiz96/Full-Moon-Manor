16ba67f782db320c5a4c5241c2f6474fe6a9cc70
Matthew Bronstein <mbronstein1@gmail.com>
2022-11-14 13:46:14 -0600

    fixed bug with repeat events and adjusted spacing of room page

--- client/src/components/Dbuttons/Dbutton.js
+++ client/src/components/Dbuttons/Dbutton.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 import './dbutton.css';
 
 export const DPad = ({ roomDirections, setEventResolution, visibility, setVisibility }) => {
-    useEffect(() => {
-        setEventResolution("");
-        setVisibility('hidden');
-        // console.log('working')
-    }, [])
+    // useEffect(() => {
+    //     setEventResolution("");
+    //     setVisibility('hidden');
+    //     // console.log('working')
+    // }, [])
     // console.log('d btn')
     return (
         <div style={{ visibility }} className="set dark">
@@ -17,11 +17,20 @@ export const DPad = ({ roomDirections, setEventResolution, visibility, setVisibi
                 <a className="right" href="#"></a>
                 <a className="down" href="#"></a>
                 <a className="left" href="#"></a> */}
-                {roomDirections.map((room, index) => {
-                    return (
-                        <Link key={`room-index: ${index}`} className={room.userDirection} to={`/room/${room.nextRoom}`} ></Link>
-                    )
-                })}
+                <div onClick={() => {
+                    setVisibility("hidden");
+                    setEventResolution("");
+                    }}>
+                    {roomDirections.map((room, index) => {
+                        return (
+                            <>
+                                <Link key={`room-index: ${index}`} className={room.userDirection} to={`/room/${room.nextRoom}`} ></Link>
+
+
+                            </>
+                        )
+                    })}
+                </div>
             </nav>
         </div>
     )
