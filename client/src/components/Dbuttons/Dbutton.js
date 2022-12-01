9226b2470dae0694f331eb4db645fbfb30e6c19d
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-01 10:47:42 -0600

    experimenting with routing by changing express get in server.js and changed visibility to display

--- client/src/components/Dbuttons/Dbutton.js
+++ client/src/components/Dbuttons/Dbutton.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from 'react-router-dom'
 import './dbutton.css';
 
-export const DPad = ({ roomDirections, setEventResolution, visibility, setVisibility }) => {
+export const DPad = ({ roomDirections, setEventResolution, display, setDisplay }) => {
 
     return (
-        <div style={{ visibility }} className="set dark">
+        <div style={{ display }} className="set dark">
             <nav className="o-pad">
                 <div onClick={() => {
-                    setVisibility("hidden");
+                    setDisplay("none");
                     setEventResolution("");
                 }}>
                     {roomDirections.map((room, index) => {
